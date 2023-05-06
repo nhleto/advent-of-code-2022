c964fd2cac04f7617073291a74a4b53740ac7a1b
Henry <hnr.leto11@gmail.com>
2023-05-06 08:10:50 -0400

    day1-p1

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,6 +1,20 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
 
-const text = fs.readFile("./input.txt", (err, data) => {
-  const bunch = data.join("/n");
-  console.log(bunch);
-});
+async function day1() {
+  const input = await fs.readFile(path.resolve(__dirname, "./input.txt"));
+  const data = input
+    .toString()
+    .split("\n\n")
+    .map((col) => [col.replace(/\n/g, " ")].map((x) => x.split(" ")).flat());
+
+  const result = data.map((col) =>
+    col.reduce((acc, cur) => (acc += parseInt(cur)), 0)
+  );
+
+  const big = Math.max(...result);
+
+  console.log(big);
+}
+
+day1();
