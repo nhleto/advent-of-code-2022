aadd4bd800d3d10c7181e2589a20564990d26ccc
Henry <hnr.leto11@gmail.com>
2023-05-06 08:22:55 -0400

    day 1 p2

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -12,9 +12,15 @@ async function day1() {
     col.reduce((acc, cur) => (acc += parseInt(cur)), 0)
   );
 
-  const big = Math.max(...result);
+  // const big = Math.max(...result);
 
-  console.log(big);
+  // console.log(big);
+
+  const big3 = result
+    .sort((a, b) => (a > b ? -1 : 1))
+    .splice(0, 3)
+    .reduce((acc, cur) => (acc += cur));
+  console.log(big3);
 }
 
 day1();
